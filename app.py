35aed8def7e023e58c79b91849e1e83bdb3ac895
Michael Jelly <53475252+michaeljelly@users.noreply.github.com>
2022-08-27 23:15:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ def init():
     lms = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
 
     model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=lms, use_auth_token=HF_AUTH_TOKEN).to("cuda")
+    def dummy(images, **kwargs):
+        return images, False
+    model.safety_checker = dummy
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
